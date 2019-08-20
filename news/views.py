cf21c1aeb251e20a1e4628763b59c68cf63ac5b1
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-20 11:42:24 +0300

    define a function that takes in a date parameter and returns a day of the week as arranged in an array

--- news/views.py
+++ news/views.py
@@ -16,3 +16,14 @@ def news_of_day(request):
             </html>
             '''
     return HttpResponse(html)
+
+def convert_dates(dates):
+    #function that gets weekday no for the date
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
+
