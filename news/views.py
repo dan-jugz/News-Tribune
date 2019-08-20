006aca2ae3737be1a95d89b254ae960c033005e4
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-20 12:57:54 +0300

    create a new view function which takes the date from the url converts it to a dateobject by using strptime function

--- news/views.py
+++ news/views.py
@@ -29,3 +29,16 @@ def convert_dates(dates):
     day = days[day_number]
     return day
 
+def past_days_news(request,past_date):
+    #converts data from the string url
+    date = dt.datetime.strptime(past_date,'%y-%m-%d').date()
+
+    day = convert_dates(date)
+    html =f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
