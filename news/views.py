a294617d61333a174f6fea51126b8dfaf3a88b19
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-20 13:02:08 +0300

    redirect to 404 error when the date value is incorrectly formatted

--- news/views.py
+++ news/views.py
@@ -33,6 +33,10 @@ def past_days_news(request,past_date):
     #converts data from the string url
     date = dt.datetime.strptime(past_date,'%y-%m-%d').date()
 
+    except ValueError:
+        #raise 404 error when valueError is thrown
+        raise Http404()
+        
     day = convert_dates(date)
     html =f'''
         <html>
