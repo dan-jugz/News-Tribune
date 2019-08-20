a0ce6e089b3f8af5c843b92fc51b2a9e46b38283
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-20 14:17:46 +0300

    remove the html on the view script and return a render template that renders the corresponding template

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
 import datetime as dt
 
@@ -10,22 +10,19 @@ def news_of_day(request):
     date =dt.date.today()
     
     return render(request, 'all-news/today-news.html',{"date":date,})
-
+#presents news from past days
 def past_days_news(request,past_date):
+
+    try:
     #converts data from the string url
-    date = dt.datetime.strptime(past_date,'%y-%m-%d').date()
+        date = dt.datetime.strptime(past_date,'%y-%m-%d').date()
 
     except ValueError:
         #raise 404 error when valueError is thrown
         raise Http404()
         assert False
 
-    day = convert_dates(date)
-    html =f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    if date == dt.date.today():
+        return redirect(news_of_day)
+
+    return render(request, 'all-news/past-news.html', {"date": date})
