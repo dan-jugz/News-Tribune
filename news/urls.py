1c73a01fd4de28a2629f8ff1d0df4dd3366cac9f
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-20 12:23:46 +0300

    correct the url to show todays news

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.welcome,name='welcome'),
-    url(r'^today',views.news_of_day,name='newsToday')
+    url(r'^today/$',views.news_of_day,name='newsToday')
 ]
