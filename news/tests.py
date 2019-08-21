574cd2eb9266170af70867b24f2c94fee88b4aba
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-21 12:51:27 +0300

    define a function to save editor in model and create a testcase for it

--- news/tests.py
+++ news/tests.py
@@ -12,3 +12,8 @@ class EditorTestClass(TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.james,Editor))
     
+    # Testing Save Method
+    def test_save_method(self):
+        self.james.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
