0e70a30bcbb1f5e4bfb552f15f663e43b064e677
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-21 17:33:52 +0300

    define a method that returns the days news

--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
 import datetime as dt
+from .models import Article
 
 # Create your views here.
 def welcome(request):
@@ -23,6 +24,11 @@ def past_days_news(request,past_date):
         assert False
 
     if date == dt.date.today():
-        return redirect(news_of_day)
+        return redirect(news_today)
 
     return render(request, 'all-news/past-news.html', {"date": date})
+
+def news_today(request):
+    date = dt.date.today()
+    news = Article.todays_news()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
