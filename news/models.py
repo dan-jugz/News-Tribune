1b6127766710024ee763c00a36eb9742ab980b04
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-21 12:20:25 +0300

    create new many to many field of the tags class since they will be in many articles

--- news/models.py
+++ news/models.py
@@ -21,3 +21,4 @@ class Article(models.Model):
     title = models.CharField(max_length =60)
     post = models.TextField()
     editor = models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)
